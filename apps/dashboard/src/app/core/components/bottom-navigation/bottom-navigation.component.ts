976acfcde360765f8c522fef6df8fe4e25b620dd
Michael <mikemountain.dev@gmail.com>
2021-06-23 22:52:28 +0200

    Create the top-navigation component

---
+++ apps/dashboard/src/app/core/components/bottom-navigation/bottom-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dash-bottom-navigation',
+  templateUrl: './bottom-navigation.component.html',
+  styleUrls: ['./bottom-navigation.component.scss']
+})
+export class BottomNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/dashboard/src/app/core/core.module.ts
+++ apps/dashboard/src/app/core/core.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../../environments/environment';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AppRoutingModule } from '../app-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
+import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
+import { BottomNavigationComponent } from './components/bottom-navigation/bottom-navigation.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    TopNavigationComponent,
+    BottomNavigationComponent
   ],
   imports: [
     BrowserModule,
